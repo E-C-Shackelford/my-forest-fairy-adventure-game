1f0de070de60bc385cc28e1fac9bbf5dd3608b20
E-C-Shackelford <98488905+E-C-Shackelford@users.noreply.github.com>
2022-04-24 18:40:48 -0700

    add code to print descriptions of what's happening in the storyline to the console, add random.choice function so villan is chosen by random each round the game is played, and add delays between messages for user readability

--- my-forest-fairy-adventure-game.py
+++ my-forest-fairy-adventure-game.py
@@ -1,2 +1,26 @@
 import time
 import random
+
+def print_pause(message, pause=2):
+    print(message)
+    time.sleep(pause)
+
+
+def random_villan():
+    villan_choice = ["troll", "werewolf", "goblin", "orc"]
+    villan = random.choice(villan_choice)
+
+
+def intro():
+    print_pause("You're in a magical forest, walking along a forest stream.")
+    print_pause("As you walk along the stream,\n"
+                "you find a beautiful blue stone.")
+    print_pause("You pick up the stone to look at it more closely.", 3)
+    print_pause("The stone is mesmerizing.")
+    print_pause("Little do you know,\n"
+                "it has very powerful properties.", 3)
+    print_pause("You approach a fork in the road.")
+    print_pause("You can either take the path to your left,\n"
+                "or that to your right.", 3)
+    print_pause("You place the stone in your sachel\n"
+                "and look at each path.")
