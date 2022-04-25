ba5f66fa18078bb5f82fe5e00115dcfd3d059149
E-C-Shackelford <98488905+E-C-Shackelford@users.noreply.github.com>
2022-04-24 18:47:46 -0700

    update code with valid_input function and define path choices
    update code by asking user what they'd like to do and gathering input from the user with valid_input function and define path choice with two path options

--- my-forest-fairy-adventure-game.py
+++ my-forest-fairy-adventure-game.py
@@ -24,3 +24,22 @@ def intro():
                 "or that to your right.", 3)
     print_pause("You place the stone in your sachel\n"
                 "and look at each path.")
+
+
+def valid_input(prompt, options):
+    response = input(prompt)
+    if response not in options:
+        print_pause("Invalid input, please try again")
+        return valid_input(prompt, options)
+    return response
+
+
+def path_choice():
+    print_pause("Which path will you take?\n"
+                "Please enter the path you would like to take:")
+    path = valid_input("1. left path\n"
+                       "2. right path\n", ["1", "2"])
+    if path == '1':
+        left_path()
+    elif path == '2':
+        right_path()
